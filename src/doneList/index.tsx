6cbf11e355d4d752dda9c1c0118583921bb8b80d
Patryk Wojtowicz <patryk.wojtowicz@sovaaccounting.pl>
2021-02-01 18:35:10 +0100

    Refactor doneList

--- src/doneList/index.tsx
+++ src/doneList/index.tsx
@@ -12,46 +12,63 @@ import { FaTrash, FaCoffee } from 'react-icons/fa'
 import { GiReceiveMoney } from 'react-icons/gi'
 import TextField from '@material-ui/core/TextField'
 import dayjs from 'dayjs'
-
 interface Props {
 	doneIntervals: DoneInterval[]
 	updateInterval: (newInterval: DoneInterval) => void
 	deleteInterval: (id: string) => void
+	title: string
 }
 const formatDate = (date: string) => dayjs(date).format('HH:mm D.MM.YY')
 
 const sortListFromNewest = (intervals: DoneInterval[]) =>
 	intervals.sort((a, b) => a.finished.localeCompare(b.finished)).reverse()
+interface DoneItemProps {
+	interval: DoneInterval
+	onUpdate: (newInterval: DoneInterval) => void
+	onDelete: (id: string) => void
+}
+const DoneItem = ({ interval, onUpdate, onDelete }: DoneItemProps) => (
+	<FinishedContainer key={interval.id}>
+		<RecordHeader>
+			<DurationLabelContainer>
+				<Icon>{interval.isWork ? <GiReceiveMoney /> : <FaCoffee />}</Icon>
+				<DateTime>{interval.duration.minutes + 'm'}</DateTime>
+			</DurationLabelContainer>
+			<DateTime>{formatDate(interval.finished)}</DateTime>
+			<TrashButton onClick={() => onDelete(interval.id)}>
+				<FaTrash />
+			</TrashButton>
+		</RecordHeader>
+		{interval.isWork ? (
+			<TextField
+				label='Description'
+				fullWidth
+				margin='normal'
+				multiline
+				variant='outlined'
+				onBlur={(e) => onUpdate({ ...interval, note: e.target.value })}
+			/>
+		) : null}
+	</FinishedContainer>
+)
 
-const DoneList = ({ doneIntervals, updateInterval, deleteInterval }: Props) => {
+const DoneList = ({
+	doneIntervals,
+	updateInterval,
+	deleteInterval,
+	title,
+}: Props) => {
 	return (
 		<>
-			<ListHeader>Done</ListHeader>
+			<ListHeader>{title}</ListHeader>
+
 			{sortListFromNewest(doneIntervals).map((interval) => (
-				<FinishedContainer key={interval.id}>
-					<RecordHeader>
-						<DurationLabelContainer>
-							<Icon>{interval.isWork ? <GiReceiveMoney /> : <FaCoffee />}</Icon>
-							<DateTime>{interval.duration.minutes + 'm'}</DateTime>
-						</DurationLabelContainer>
-						<DateTime>{formatDate(interval.finished)}</DateTime>
-						<TrashButton onClick={() => deleteInterval(interval.id)}>
-							<FaTrash />
-						</TrashButton>
-					</RecordHeader>
-					{interval.isWork ? (
-						<TextField
-							label='Description'
-							fullWidth
-							margin='normal'
-							multiline
-							variant='outlined'
-							onBlur={(e) =>
-								updateInterval({ ...interval, note: e.target.value })
-							}
-						/>
-					) : null}
-				</FinishedContainer>
+				<DoneItem
+					key={interval.id}
+					interval={interval}
+					onUpdate={updateInterval}
+					onDelete={deleteInterval}
+				/>
 			))}
 		</>
 	)
